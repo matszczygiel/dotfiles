c436801f8ecd05cd011a583099492e7664c56dc9
Mateusz Szczygieł <msz@genesismobo.com>
2021-05-27 15:55:00 +0200

    add starship config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,2 @@
-alias aptu="sudo apt update && sudo apt upgrade && sudo apt autoremove && sudo apt autoclean"
-alias apti="sudo apt install $1"
-
 zoxide init fish | source
-
+starship init fish | source
